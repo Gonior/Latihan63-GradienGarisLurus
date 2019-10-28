925d536eb573a050ec62c760ab59b72126e71484
Gonior <43252085+Gonior@users.noreply.github.com>
2019-10-28 07:10:29 +0700

    Dibuat : Senin, 28 Oktober 2019

---
+++ src/pbo11k/pkg10118901/latihan63/gradiengarislurus/GarisLurus.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo11k.pkg10118901.latihan63.gradiengarislurus;
+/**
+ * @author
+ * NAMA             : Dedi cahya
+ * NIM              : 10118901
+ * KELAS            : IF11K
+ * Deskripsi Program  : Program untuk menampilkan menghitung gradien garis lurus dengan inteface
+ * 
+ */
+public interface GarisLurus {
+   public int hitungGradien(); 
+}
